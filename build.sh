fe3ebd3a31f4dcef2769e879bc23c7ee40156ee5
Christopher Villamarín <alexpila.314@gmail.com>
2023-07-17 16:39:47 -0500

    add build command to deploy

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ set -o errexit
 
 pip3 install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
